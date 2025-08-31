beefdc6fd4d06914e8f36739a3db0e144ce76ae8
Sofierma <Sofiermas.tech@gmail.com>
2025-08-31 18:47:06 +0300

    solving homework 1-map: v.2

--- 1-map.js
+++ 1-map.js
@@ -4,13 +4,28 @@ const array1 = [
   {id: 1, name: 'Вася'},
 ];
 
-const setArray = new Set();
-const filterArray = array1.filter((el) => {
-  if (!setArray.has(el.id)) {
-    setArray.add(el.id);
-    return true;
-  }
-  return false;
+// Решение 2
+const uniqueArray = array1.map((el) => {
+  return el.id;
 });
+console.log(uniqueArray);
 
-console.log(filterArray);
+const setArray = new Set(uniqueArray);
+console.log(setArray);
+
+const resultArray = Array.from(setArray).map(id => {
+  return array1.find(user => user.id === id);
+});
+console.log(resultArray);
+
+// Решение 1
+// const setArray = new Set();
+// const filterArray = array1.filter((el) => {
+//   if (!setArray.has(el.id)) {
+//     setArray.add(el.id);
+//     return true;
+//   }
+//   return false;
+// });
+
+// console.log(filterArray);
