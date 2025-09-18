0adbd540ba7e73267ff685f70610b5eb0e202244
Sofierma <Sofiermas.tech@gmail.com>
2025-09-18 21:43:15 +0300

    solving task 'class Car'continue

--- 6-class.js
+++ 6-class.js
@@ -29,3 +29,49 @@ car.info();
 car.mileage = 500;
 console.log(car.mileage);
 car.info();
+
+// --------------------
+
+class Enemy {
+  health;
+
+  constructor(health) {
+    this.health = health;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    console.log(this.health);
+  }
+}
+
+class Sword {
+  #damage;
+
+  constructor(damage) {
+    this.#damage = damage;
+  }
+
+  strike(enemy) {
+    enemy.receiveDamage(this.#damage);
+  }
+}
+
+class Orc extends Enemy {
+  constructor(health) {
+    super(health);
+  }
+
+  receiveDamage(damage) {
+    if (Math.random() > 0.5) {
+      this.health = this.health - damage;
+    }
+    console.log(this.health);
+  }
+}
+
+const enemy = new Orc(10);
+const sword = new Sword(3);
+
+sword.strike(enemy);
+sword.strike(enemy);
