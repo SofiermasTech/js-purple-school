6e15ec420f7e186806142e153066a60afc687418
Sofierma <Sofiermas.tech@gmail.com>
2025-09-24 12:40:29 +0300

    solving task with solid

---
+++ 8-solid.js
@@ -0,0 +1,40 @@
+class Billing {
+  amount;
+
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  calculateTotal() {
+    return this.amount;
+  }
+}
+
+class hourBilling extends Billing {
+
+  constructor(amount, hours) {
+    super(amount);
+    this.hours = hours;
+  }
+
+  calculateTotal() {
+    return this.amount * this.hours;
+  }
+}
+
+class itemBilling extends Billing {
+
+  constructor(amount, items) {
+    super(amount);
+    this.items = items;
+  }
+
+  calculateTotal() {
+    return this.amount * this.items;
+  }
+}
+
+const item = new itemBilling(5, 4)
+
+console.log(item)
+console.log(item.calculateTotal())
